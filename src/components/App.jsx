ba335d827365bdea7f3c2691b10180c85ad458f9
AliceChpmnk <alisablog50@gmail.com>
2023-03-14 22:06:26 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,19 +6,9 @@ import Contacts from './Contacts/Contacts';
 import Filter from './Filter/Filter';
 import { StyledForm } from './Form/Form.styled';
 
-const testContacts = [{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },]
-
 function App() {
   const [contacts, setContacts] = useState(JSON.parse(localStorage.getItem('contacts')) ?? []);
   const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    console.log(`Contacts to test: `);
-    console.log(testContacts);
-  }, []) 
 
   useEffect(()=> {
     localStorage.setItem('contacts', JSON.stringify(contacts));
